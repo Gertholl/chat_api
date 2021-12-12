8083bc7d21c86bfb6e1d4ec3dce79010eba9c4f9
Gertholl <gertholl1337@gmail.com>
2021-12-12 18:58:14 +0700

    tess

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from routes.routes import *
 # from routes.routes import init_api
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
+   
     db.create_all(app=app)
     init_api(app, db)
     app.run(debug=False, port=9000)
